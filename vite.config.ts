59a1026a6f347b83382f32c233fac0aa4bca7e81
AIMERIC-DECAE <decaeaimeric@gmail.com>
2025-09-29 21:31:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/site.internet.aquadrop/',
+  base: '/site.internet.aquadrop/', //
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
